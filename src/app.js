bdfae89d662d0117d97aef0b513f4626f33d9b51
javiscripto <javier.mecker94@gmail.com>
2023-12-12 22:48:46 -0300

    integracion de logger winston

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import errorHandler from "./middlewares/index.js"
+import { addLoggerMiddleware , logger} from "../utils/logger.js";
 
 const app = express();
 
@@ -7,10 +8,13 @@ const port = 8080;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json())
+app.use(addLoggerMiddleware);
+
 
 
 import mockRouter from "../src/routes/mocking.js";
 import productsRouter from "./routes/products.js"
+
 
 app.use("/api/mockingProducts",mockRouter)
 app.use("/api/products", productsRouter)
@@ -19,5 +23,6 @@ app.use(errorHandler)
 
 
 app.listen(port, ()=>{
-    console.log(`servidor corriendo en el puerto ${port}`)
+
+    logger.info(`servidor corriendo en el puerto ${port}`)
 })
